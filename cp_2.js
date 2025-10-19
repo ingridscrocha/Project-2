fcbec21c4793949136209b5af6a59e2c0decc2dd
Ingrid Rocha <ingrid22@usf.edu>
2025-10-19 15:26:21 -0400

    Add fetchProductsAsync()

--- cp_2.js
+++ cp_2.js
@@ -10,3 +10,13 @@ function fetchProductsThen() {
       console.log("Fetch failed:", error); // Log fetch error
     });
 }
+
+async function fetchProductsAsync() {
+  try {
+    const response = await fetch('https://www.course-api.com/javascript-store-products');
+    const data = await response.json();
+    displayProducts(data); // Pass products to the next function
+  } catch (error) {
+    handleError(error); // If something goes wrong
+  }
+}
